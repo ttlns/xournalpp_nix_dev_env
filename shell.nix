e9e97139e1361e4587446e1df5b69daba8c5b267
Steve <steve@ttlns.ch>
2026-08-31 07:41:13 +0200

    fix sha

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
   pkgs = import (fetchTarball{ 
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/26.05.tar.gz";
-    sha256 = "sha256:0ax8m8l20zd6hrbylrvlgs1inzd4nld8x4kh7r8k11inz7nfdjmw";
+    sha256 = "sha256:0am8xx09fx5yf2p0wb001v0jx1g5hrfb76h4r37xph378jgk7pcr";
   }) {};
 in
 pkgs.mkShell {
